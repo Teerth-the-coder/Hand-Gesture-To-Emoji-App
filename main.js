66f800af599908b974aaabdb28fe3002d960c529
Teerth-the-coder <154464588+Teerth-the-coder@users.noreply.github.com>
2024-07-15 18:21:10 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function check() {
-   speak() 
+    img = document.getElementById("snap");
+    classifier.classify(img, gotResults);
 }
 
 function capture() {
@@ -17,19 +18,54 @@ function speak() {
 p1 = "";
 p2 = "";
 
-function gotResults() {
-    
+function gotResults(error, result) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(result);
+        p1 = result[0].label;
+        p2 = result[1].label;
+
+
+        if (p1 == "Rockstar") {
+            document.getElementById("emoji1").innerHTML = "🤟"
+        } else if (p1 == "Thumbs Up") {
+            document.getElementById("emoji1").innerHTML = "👍"
+        } else if (p1 == "Thumbs down") {
+            document.getElementById("emoji1").innerHTML = "👎"
+        } else if (p1 == "Peace") {
+            document.getElementById("emoji1").innerHTML = "✌️"
+        }
+
+        if (p2 == "Rockstar") {
+            document.getElementById("emoji2").innerHTML = "🤟"
+        } else if (p2 == "Thumbs Up") {
+            document.getElementById("emoji2").innerHTML = "👍"
+        } else if (p2 == "Thumbs down") {
+            document.getElementById("emoji2").innerHTML = "👎"
+        } else if (p2 == "Peace") {
+            document.getElementById("emoji2").innerHTML = "✌️"
+        }
+
+        speak()
+    }
+
+
 }
 
+//https://teachablemachine.withgoogle.com/models/pNfju-wZz3/json
+
 Webcam.set({
-    width:300,
-    height:300,
-    image_format:'png',
-    png_quality:90
+    width: 300,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
 });
 
 camera = document.getElementById("camera");
 
 Webcam.attach('#camera');
 
-classifier = ml5.imageClassifier("", () => console.log("model has loaded"))
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/pNfju-wZz3/model.json", () => console.log("model has loaded"))
+
+//https://teachablemachine.withgoogle.com/models/x5HFYkpHo/json
